8a1cdc3783baff590bcca5898832febdb06ad43f
Hugo Hsi <hhsi@hugodehsign.com>
2024-07-15 13:01:00 -0400

    protected dashboard routes for users that have not added at least one player

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,12 +1,16 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
 import { Outlet, redirect } from '@remix-run/react';
 import { $path } from 'remix-routes';
+import { getPrimaryPlayer } from '~/services/auth/getPlayers.server';
 import validateRequest from '~/services/auth/validateRequest.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const { session } = await validateRequest(request);
 
   if (!session) throw redirect($path('/login'));
+
+  const { primaryPlayer } = await getPrimaryPlayer(request);
+  if (!primaryPlayer) throw redirect($path('/get-started'));
 
   return null;
 }
